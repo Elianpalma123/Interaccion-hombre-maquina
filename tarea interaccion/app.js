22caa7825f178e7857fc4360e0b03d26d41a85f8
Elianpalma123 <elianpalmasanchez@gmail.com>
2026-05-29 08:20:02 -0500

    Add files via upload

---
+++ tarea interaccion/app.js
@@ -0,0 +1,52 @@
+const app = Vue.createApp({
+
+    data() {
+
+        return {
+
+            nuevaTarea: "",
+
+            tareas: []
+
+        }
+
+    },
+
+    methods: {
+
+        agregarTarea() {
+
+            if (this.nuevaTarea.trim() !== "") {
+
+                this.tareas.push({
+
+                    texto: this.nuevaTarea,
+
+                    completada: false
+
+                })
+
+                this.nuevaTarea = ""
+
+            }
+
+        },
+
+        eliminarTarea(index) {
+
+            this.tareas.splice(index, 1)
+
+        },
+
+        toggleTarea(index) {
+
+            this.tareas[index].completada =
+                !this.tareas[index].completada
+
+        }
+
+    }
+
+})
+
+app.mount("#app")
